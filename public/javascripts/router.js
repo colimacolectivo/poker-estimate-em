02ec3c30e4e80db8acab17f0f7bb354372dc722f
Antonio Chavez <cavjzz@gmail.com>
2012-11-09 19:37:55 -0600

    Fixed issues with the initialize and put the index route.

--- public/javascripts/router.js
+++ public/javascripts/router.js
@@ -1,9 +1,14 @@
 TXE.Router = Backbone.Router.extend({
   routes: {
-    "" : "initialize"
+    "" : "index"
   },
 
   initialize: function(){
     console.log("Hello I'm working!");
+  },
+
+  index: function(){
+    console.log("We're on Index page!");
   }
+
 });
