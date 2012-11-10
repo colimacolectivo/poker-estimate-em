170c8be7216b75f0dbb9647c1487713a3fbd3f1b
Antonio Chavez <cavjzz@gmail.com>
2012-11-10 16:47:30 -0600

    added binding for create game

--- public/javascripts/app/views/indexView.js
+++ public/javascripts/app/views/indexView.js
@@ -8,20 +8,25 @@ TXE.Views.indexView = Backbone.View.extend({
   collection: new TXE.Collections.projectsCollection(),
 
   events: {
+    "click .fn-createGame" : "creatingGame"
   },
 
   initialize: function(){
     this.collection.bind("reset", this.render, this);
     this.collection.fetch();
     this.user_logged_in = eval($("#user_logged_in").val());
+  },
+
+  render: function(){
+    //if (this.user_logged_in) {
+      //var context = this.collection.toJSON();
+      //this.$('#projects').html(this.projectsTemplate({context: context}));
+    //} else {
+      //this.$('#projects').html(this.userLoginTemplate);
+    //}
+  },
+
+  creatingGame: function(e){
   }
 
-  // render: function(){
-  //   if (this.user_logged_in) {
-  //     var context = this.collection.toJSON();
-  //     this.$el.html(this.projectsTemplate({context: context}));
-  //   } else {
-  //     this.$el.html(this.userLoginTemplate);
-  //   }
-  // }
 });
