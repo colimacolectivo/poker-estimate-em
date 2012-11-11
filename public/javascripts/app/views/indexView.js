f831da8d53a1a862a8daf3cbb0bfe4724020a97f
sace <jaime.gonzalez@tangosource.com>
2012-11-10 18:08:41 -0600

    commented render created by tono

--- public/javascripts/app/views/indexView.js
+++ public/javascripts/app/views/indexView.js
@@ -14,14 +14,14 @@ TXE.Views.indexView = Backbone.View.extend({
     this.collection.bind("reset", this.render, this);
     this.collection.fetch();
     this.user_logged_in = eval($("#user_logged_in").val());
-  },
-
-  render: function(){
-    if (this.user_logged_in) {
-      var context = this.collection.toJSON();
-      this.$el.html(this.projectsTemplate({context: context}));
-    } else {
-      this.$el.html(this.userLoginTemplate);
-    }
   }
+
+  // render: function(){
+  //   if (this.user_logged_in) {
+  //     var context = this.collection.toJSON();
+  //     this.$el.html(this.projectsTemplate({context: context}));
+  //   } else {
+  //     this.$el.html(this.userLoginTemplate);
+  //   }
+  // }
 });
