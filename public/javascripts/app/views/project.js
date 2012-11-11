0b10c8e075b3f456f0115119c5d119b1d0fa3876
Eduardo Ruiz <eduarbo@gmail.com>
2012-11-11 12:05:28 -0800

    updated

--- public/javascripts/app/views/project.js
+++ public/javascripts/app/views/project.js
@@ -3,11 +3,12 @@ TXE.Views.Project = Backbone.View.extend({
   el: "#main-content",
   
   initialize: function() {
+    this.template = _.template($("#project-view").html());
     this.render();
   },
 
   render: function() {
-    this.$el.html('');
+    this.$el.html(this.template());
   }
 
 });
