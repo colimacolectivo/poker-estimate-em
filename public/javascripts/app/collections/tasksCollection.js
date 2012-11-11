2c7053283836c1c3a1e69cafffa43494314e64bd
Eduardo Ruiz <eduarbo@gmail.com>
2012-11-11 12:21:27 -0800

    updated asdasd

--- public/javascripts/app/collections/tasksCollection.js
+++ public/javascripts/app/collections/tasksCollection.js
@@ -1,8 +1,11 @@
 TXE.Collections.tasksCollection = Backbone.Collection.extend({
   // This collection allows you to instantiate the object with the project id allowing them make only requests or get information from the API related to that project
-  
-  initialize: function(projectId){
-    this.url = '/api/v1/projects/' + projectId + '/tasks';
+  initialize: function(pid) {
+    this.project_id = pid || 0;
+  },
+
+  url: function() {
+    return '/api/v1/projects/' + this.project_id + '/tasks';
   }
 
 });
--- public/javascripts/app/views/project.js
+++ public/javascripts/app/views/project.js
@@ -4,11 +4,17 @@ TXE.Views.Project = Backbone.View.extend({
   
   initialize: function() {
     this.template = _.template($("#project-view").html());
-    this.render();
+    a=this.tasksCollection = new TXE.Collections.tasksCollection(this.model.get('id'))
+
+    this.tasksCollection.fetch();
+
+    this.tasksCollection.on('reset', this.render, this);
   },
 
   render: function() {
-    this.$el.html(this.template());
+    this.$el.html(this.template({
+      project: this.model.toJSON()
+    }));
   }
 
 });
