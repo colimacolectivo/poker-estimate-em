ac0b7f0fd074cd7e2024f4523669fe96634f7429
Narciso Guillen <narciso.guillen@tangosource.com>
2012-11-11 12:19:41 -0800

    Show games

--- public/javascripts/app/views/project.js
+++ public/javascripts/app/views/project.js
@@ -4,7 +4,21 @@ TXE.Views.Project = Backbone.View.extend({
   
   initialize: function() {
     this.template = _.template($("#project-view").html());
+    var projectId = this.model.get('id');
+    this.games = new TXE.Collections.gamesCollection(projectId);
+    this.games.bind('reset', this.showGames, this);
+    this.games.fetch();
     this.render();
+  },
+
+  showGames: function(){
+    var gameList = this.$('.fn-game-input');
+    var title = "";
+
+    _.each(this.games.models, function(model){
+      title=model.get('name');
+      gameList.prepend("<li>"+title+"</li>");
+    });
   },
 
   render: function() {
