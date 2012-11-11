e60c4c837a8b296549576f2ad8b94e623687b536
Eduardo Ruiz <eduarbo@gmail.com>
2012-11-11 13:25:08 -0800

    added play and delete methods to game item view

--- public/javascripts/app/models/game.js
+++ public/javascripts/app/models/game.js
@@ -1,4 +1,7 @@
 TXE.Models.Game = Backbone.Model.extend({
   idAttribute: "_id",
-  url: '/api/v1/projects/:projectId/games/:id'
+
+  url: function() {
+    return  "/api/v1/projects/" +this.get('project_id')+ "/games/" +this.get('_id');
+  }
 });
