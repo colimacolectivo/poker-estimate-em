11a6c53278291c3c52cd7fcab9785d435e5d5c8f
Antonio Chavez <cavjzz@gmail.com>
2012-11-10 22:55:15 -0600

    Added account to template

---
+++ public/javascripts/app/views/projectGamesView.js
@@ -0,0 +1,22 @@
+TXE.Views.projectGamesView = Backbone.View.extend({
+
+  el: "#work-area",
+
+  events:{
+  },
+
+  initialize: function(){
+    console.log("Work it!");
+  }
+//Add a "Create Game" button
+//Show a list of games
+//games could be delated
+//Games Name could be edited
+//When a game is played it should be showed disabled.
+//Show a list of tasks
+//As a user I'm able to drag tasks and drop them into a game
+//Multiple selection of tasks
+//A select All button for tasks
+//When a task is dropped to a game it should dissapear from tasks lists
+ 
+});
