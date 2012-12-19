8c130ec90e357e433a73e0e7f1c5ba4ff1370504
noelrock333 <noel_ska15@hotmail.com>
2012-12-19 13:59:26 -0600

    Added description place

--- public/javascripts/app/views/game.js
+++ public/javascripts/app/views/game.js
@@ -12,18 +12,21 @@ TXE.Views.Game = Backbone.View.extend({
 
     this.model.bind('change', this.render, this);
     this.model.fetch();
+
   },
 
   render: function(){
     var tasks = this.model.get('tasks') || [];
     var name = this.model.get('name');
+    //var description= this.model.get('description');
+    //console.log(description);
     var $list = this.$(".list");
 
     this.$('.results .three').text(name);
 
     _.each(tasks, function(task){ 
       var id = task.id;
-      $list.prepend("<li class='item'>"+id+"</li>");
+      $list.prepend("<li class='item'>"+name+" - "+id+"<div class='description'></div></li>");
     });
 
   }
