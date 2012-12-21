e9a0173223c9a4b6ad9f797abd198ceb2ca9ebd6
noelrock333 <noel_ska15@hotmail.com>
2012-12-20 19:09:32 -0600

    Changed description model properties

--- public/javascripts/app/models/description.js
+++ public/javascripts/app/models/description.js
@@ -1,7 +1,7 @@
 TXE.Models.Description = Backbone.Model.extend({
-  idAttribute: "_id",
+  idAttribute: "task_id",
 
   url: function() {
-    return  "/api/v1/projects/" +this.get('project_id')+ "/tasks/" +this.get('_id');
+    return  "/api/v1/projects/" +this.get('project_id')+ "/tasks/" +this.get('task_id');
   }
 });
